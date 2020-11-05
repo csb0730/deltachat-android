d393e0eb207f5fccfb91bb351440f583c357bd79
csb0730 <schneider17@gmx.de>
2020-11-05 21:07:39 +0100

    switch off logging in AttachmentRegionDecoder

--- src/org/thoughtcrime/securesms/components/subsampling/AttachmentRegionDecoder.java
+++ src/org/thoughtcrime/securesms/components/subsampling/AttachmentRegionDecoder.java
@@ -27,7 +27,8 @@ public class AttachmentRegionDecoder implements ImageRegionDecoder {
 
   @Override
   public Point init(Context context, Uri uri) throws Exception {
-    Log.w(TAG, "Init!");
+    // cs: switch off logging in this class
+    //Log.w(TAG, "Init!");
     if (!PartAuthority.isLocalUri(uri)) {
       passthrough = new SkiaImageRegionDecoder();
       return passthrough.init(context, uri);
@@ -43,7 +44,7 @@ public class AttachmentRegionDecoder implements ImageRegionDecoder {
 
   @Override
   public Bitmap decodeRegion(Rect rect, int sampleSize) {
-    Log.w(TAG, "Decode region: " + rect);
+    //Log.w(TAG, "Decode region: " + rect);
 
     if (passthrough != null) {
       return passthrough.decodeRegion(rect, sampleSize);
@@ -65,7 +66,7 @@ public class AttachmentRegionDecoder implements ImageRegionDecoder {
   }
 
   public boolean isReady() {
-    Log.w(TAG, "isReady");
+    //Log.w(TAG, "isReady");
     return (passthrough != null && passthrough.isReady()) ||
            (bitmapRegionDecoder != null && !bitmapRegionDecoder.isRecycled());
   }
